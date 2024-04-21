bf3fc5e2613c2359a2ee811bac6984324049f723
Bill Grenard <williamgrenard@gmail.com>
2024-04-21 12:23:01 -0700

    Reject input not in the range 0-100

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,7 +4,16 @@ const pixelColor = 'aquamarine';
 const penColor = 'cadetblue';
 
 button.addEventListener('click', () => {
-    let length = prompt('Choose the number of pixels per side.');
+    let length = prompt('Choose the number of pixels per side. (max 100)');
+    
+    //We need to reject input that is not a number, any empty input, and any number not in the range 0-100.
+    while (Number(length) !== Number(length) ||
+            Number(length) === null || 
+            Number(length) === 0 || 
+            Number(length) > 100
+        ) {
+        length = prompt('Please type in a number between 1 and 100')
+    }
     length = Number(length)
     removeGrid();
     createGrid(length);
