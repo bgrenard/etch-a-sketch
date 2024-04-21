14d22dddd6113b54fead6c0ff1c16e311fe5d4b1
Bill Grenard <williamgrenard@gmail.com>
2024-04-21 11:49:01 -0700

    Move color logic to js file and add button to prompt for new grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,19 +1,38 @@
 const canvas = document.querySelector('.container');
+const button = document.querySelector('button');
+const pixelColor = 'aquamarine';
+const penColor = 'cadetblue';
+
+button.addEventListener('click', () => {
+    prompt('Choose the number of pixels per side.');
+})
 
 function createPixel() {
     const pixel = document.createElement('div');
+    let x = 100/16;
     pixel.classList.add('pixel');
-    canvas.appendChild(pixel);
+    pixel.style.flexBasis = `${x}%`;
+    pixel.style.backgroundColor = pixelColor;
 
-    pixel.addEventListener('mouseenter', (e) => {
-        pixel.style.backgroundColor = 'cadetblue';
+    pixel.addEventListener('mouseenter', () => {
+        pixel.style.backgroundColor = penColor;
     })
 
-    pixel.addEventListener('mouseleave', (e) => {
-        pixel.style.backgroundColor = 'aquamarine';
+    canvas.appendChild(pixel);
+}
+
+function resetPixel() {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((pixel) => {
+        pixel.style.backgroundColor = pixelColor;
     })
 }
 
-for (let i = 0; i < 16; i++) {
+function resetGrid() {
+    let response = prompt('Choose the number of pixels per side.');
+
+}
+
+for (let i = 0; i < 256; i++) {
     createPixel();   
 }
